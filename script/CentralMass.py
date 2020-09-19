fc49d714ee769fa5f79690d91764d64effe3efe8
PanLing1994 <pling@localhost.localdomain>
2020-09-19 15:54:46 +0800

    central mass frame

--- script/CentralMass.py
+++ script/CentralMass.py
@@ -3,7 +3,7 @@ import math
 import	numpy	as	np
 
 
-with open("X_3872.txt","r")as f:
+with open("4Pentaquarks_cut_follow.txt","r")as f:
     msm = f.readlines()
     f.close()
 
@@ -17,8 +17,8 @@ while i < len(msm):
         #b = re.findall("\d+.\d+",msm[i+2])
 
 
-        a = re.findall(r'\d+\.?\d*',msm[i])
-        b = re.findall(r'\d+\.?\d*',msm[i+2])
+        a = re.findall(r'-?\d+\.?\d*',msm[i])
+        b = re.findall(r'-?\d+\.?\d*',msm[i+2])
 
         print (a)
         print (b)
@@ -27,29 +27,19 @@ while i < len(msm):
         d = float(a[11])-float(b[11])    
         f = float(a[12])-float(b[12])
         e = float(math.sqrt(c*c+d*d+f*f))  #ralatitive momentum
-        print"relatitive momentum"
         print(e)
-        n = float(a[10])*float(a[10])+float(a[11])*float(a[11])+float(a[12])*float(a[12])
-        f = float(math.sqrt(n))  #|D0|
-        print"D0 absolute value"
-        print(f)
-        o = float(b[10])*float(b[10])+float(b[11])*float(b[11])+float(b[12])*float(b[12])
-        g = float(math.sqrt(o))  #|D*0|
-        print"D*_bar0 absolute value"
-        print(g)
-        h = float(float(a[10])*float(b[10])+float(a[11])*float(b[11])+float(a[12])*float(b[12]))
-        j = float(h/(g*f))   #costha
-        print"theta"
-        print(j)
-        p = float(2*f*g*j*j)
-        print(p)
-        k = float(math.sqrt(float(a[14])*float(a[14])+float(b[14])*float(b[14])+2*float(a[13])*float(b[13])-p)) #central mass energy
-        print"cm energy"
-        print(k)
-        m = float(e*float(b[14])/k)
-        print (m)
+        h = float(b[14])
+        m = float(a[13])-float(b[13])
+        print(m)
+        n = float(a[14])*float(a[14])+float(b[14])*float(b[14])+2*float(m)*float(h)
+        o = float(math.sqrt(n))
+        print o
+        g = float(e*h/o)
 
-        if m < 0.3:
+
+        print (g)
+
+        if g < 1:
          
             event2.append(msm[i])
             #event2.extend(list2)
@@ -61,7 +51,7 @@ while i < len(msm):
 #print (event2)
 
 num2 = 0
-f2 = open("p_star.txt","w")
+f2 = open("p_star1","w")
 for line2 in event2:
     f2.write(str(line2)+"\n")
     num2 +=1
